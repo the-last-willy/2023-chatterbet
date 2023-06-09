4e6f9ce09c597e053e7d13d56963ff7fb071aa7c
the-last-willy <willy.jacquet@outlook.com>
2023-06-09 09:13:41 +0200

    [go](clean) Reword maybe tests

--- go/maybe/maybe_test.go
+++ go/maybe/maybe_test.go
@@ -8,13 +8,13 @@ import (
 	"testing"
 )
 
-func TestMaybeCanBeNothing(t *testing.T) {
+func Test_maybe_can_be_empty(t *testing.T) {
 	m := maybe.Nothing[any]()
 	_, h := m.Value()
 	assert.False(t, h)
 }
 
-func TestMaybeCanBeJustAValue(t *testing.T) {
+func Test_maybe_can_have_value(t *testing.T) {
 	m := maybe.Just(7)
 	v, has := m.Value()
 	assert.True(t, has)
